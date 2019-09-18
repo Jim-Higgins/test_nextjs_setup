fc44fcf76d1d464a383e2efc5c814e608771c420
Jim Higgins <Jim.Higgins@onthebeach.co.uk>
2019-09-18 15:53:05 +0100

    basic dynamic pages added

--- hello-next/pages/index.js
+++ hello-next/pages/index.js
@@ -1,8 +1,22 @@
 import Layout from "../components/myLayout";
+import Link from "next/link";
 
-const Index = () => (
-  <Layout>
-    <p>Hello Next.js</p>
-  </Layout>
+const Postlink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
 );
-export default Index;
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <Postlink title="Hello Next.js" />
+        <Postlink title="Learn Next.js and that jazz" />
+        <Postlink title="Deploy apps and stuff like that" />
+      </ul>
+    </Layout>
+  );
+}
